5dfb4a8d49a6d2c7d11baf84f3a5ee41ff3c12ce
Vitor Markis <vitormarkis2369@gmail.com>
2023-10-28 22:12:44 -0300

    feat: finish games available section

---
+++ apps/web/src/components/layouts/GamesAvaiable/index.tsx
@@ -0,0 +1,63 @@
+import React from "react"
+import { cn } from "@/lib/utils"
+import { ButtonPrimary } from "@/components/theme/button-primary"
+import st from "./pages.module.css"
+import { SVGWhiteDots } from "@/components/svgs/white-dots"
+
+export type GamesAvailableSectionProps = React.ComponentPropsWithoutRef<"section"> & {}
+
+export const GamesAvailableSection = React.forwardRef<
+  React.ElementRef<"section">,
+  GamesAvailableSectionProps
+>(function GamesAvailableSectionComponent({ className, ...props }, ref) {
+  return (
+    <section
+      {...props}
+      className={cn("relative overflow-hidden flex w-screen grow flex-wrap gap-6 bg-slate-950", className)}
+      ref={ref}
+    >
+      <div className="max-w-8xl mx-auto w-full h-full absolute inset-0">
+        <div
+          className="absolute inset-0"
+          style={{
+            WebkitMaskImage: "linear-gradient(101deg, transparent 43%, black 100%)",
+            maskImage: "linear-gradient(101deg, transparent 43%, black 100%)",
+          }}
+        >
+          <img
+            src="https://www.techspot.com/articles-info/1429/images/2017-06-25-image.jpg"
+            className="fillimg object-cover opacity-80"
+          />
+        </div>
+      </div>
+      <div className="relative pt-14 max-w-7xl w-full mx-auto flex justify-between z-10">
+        <div
+          className="absolute h-[1px] left-0 top-0 w-[30rem]"
+          style={{
+            background: "linear-gradient(90deg, transparent, hsla(var(--accent) / 50%), transparent)",
+          }}
+        ></div>
+        <div
+          className="absolute z-[9] h-[31rem] w-[10rem] bg-[hsl(var(--accent))] saturate-50 blur-[76px] rounded-[100%] opacity-40"
+          style={{
+            transform: "rotate(28deg) translate(1rem, -14rem)",
+          }}
+        />
+        <div className="flex flex-col relative z-10">
+          <span className="block font-semibold text-[5rem]/none">São mais de</span>
+          <span className="block font-black text-[7rem]/none">98.808 jogos</span>
+          <span className="block font-semibold text-[4rem]/none">disponíveis!</span>
+        </div>
+      </div>
+      <div className="flex w-full relative z-20 gap-20 pt-20 justify-center">
+        <ButtonPrimary colorScheme="purple-blue">Ver planos</ButtonPrimary>
+      </div>
+      <div className="w-full h-[27rem]"></div>
+      <div className={st.divisor}>
+        <SVGWhiteDots />
+      </div>
+    </section>
+  )
+})
+
+GamesAvailableSection.displayName = "GamesAvailableSection"
--- apps/web/src/pages/index.tsx
+++ apps/web/src/pages/index.tsx
@@ -1,38 +1,16 @@
 import React from "react"
 import { HeroSection } from "@/components/layouts/Hero"
 import { HowItWorksSection } from "@/components/layouts/HowItWorks"
-import st from "./pages.module.css"
 import { Header } from "@/components/layouts/Header"
-import { SVGWhiteDots } from "@/components/svgs/white-dots"
 import { GetServerSideProps } from "next"
 import { AuthSessionParams } from "@/types/UserSession"
 import { getAuthSession } from "@/util/getAuthSession"
+import { GamesAvailableSection } from "@/components/layouts/GamesAvaiable"
 
 export const getServerSideProps: GetServerSideProps = async ctx => {
   const props = await getAuthSession(ctx.req)
   return { props }
 }
-
-const images = [
-  {
-    src: "https://hourboost.com.br/cdn/steam/apps/2319770/header.jpg",
-  },
-  {
-    src: "https://hourboost.com.br/cdn/steam/apps/2055410/header.jpg",
-  },
-  {
-    src: "https://hourboost.com.br/cdn/steam/apps/2330160/header.jpg",
-  },
-  {
-    src: "https://hourboost.com.br/cdn/steam/apps/2350540/header.jpg",
-  },
-  {
-    src: "https://hourboost.com.br/cdn/steam/apps/2357390/header.jpg",
-  },
-  {
-    src: "https://hourboost.com.br/cdn/steam/apps/2212110/header.jpg",
-  },
-]
 
 export default function Home({ authSession }: AuthSessionParams) {
   return (
@@ -40,79 +18,7 @@ export default function Home({ authSession }: AuthSessionParams) {
       <Header user={authSession.user} />
       <HeroSection />
       <HowItWorksSection />
-      <section className="relative flex w-screen grow flex-wrap gap-6 py-14 bg-slate-900">
-        <div className="max-w-7xl w-full mx-auto flex justify-between">
-          <div className="flex flex-col absolute top-14">
-            <span className="block font-semibold text-[5rem]/none">São mais de</span>
-            <span className="block font-black text-[7rem]/none">98.808 jogos</span>
-            <span className="block font-semibold text-[4rem]/none">disponíveis!</span>
-          </div>
-          <div className="h-[34rem] flex flex-col justify-end items-end w-full">
-            <div className="pr-32 relative">
-              <div className="w-[30rem] z-[15] aspect-video border border-white relative">
-                <img
-                  src={images[0].src}
-                  className="fillimg object-cover"
-                  alt=""
-                />
-              </div>
-              <div className="w-[20rem] top-0 left-0 z-10 translate-x-[25rem] translate-y-[5.6rem] skew-x-[-30deg] opacity-10 aspect-video border border-white absolute">
-                <img
-                  src={images[4].src}
-                  className="fillimg object-cover"
-                  alt=""
-                />
-              </div>
-              <div className="w-[21rem] top-0 left-0 z-10 translate-x-[20rem] translate-y-[5rem] skew-x-[-30deg] opacity-25 aspect-video border border-white absolute">
-                <img
-                  src={images[3].src}
-                  className="fillimg object-cover"
-                  alt=""
-                />
-              </div>
-              <div className="w-[24rem] top-0 left-0 z-10 translate-x-[14rem] translate-y-[3.3rem] skew-x-[-14deg] opacity-50 aspect-video border border-white absolute">
-                <img
-                  src={images[2].src}
-                  className="fillimg object-cover"
-                  alt=""
-                />
-              </div>
-              <div className="w-[27rem] top-0 left-0 z-10 translate-x-[8rem] translate-y-[1.7rem] skew-x-[-6deg] opacity-75 aspect-video border border-white absolute">
-                <img
-                  src={images[1].src}
-                  className="fillimg object-cover"
-                  alt=""
-                />
-              </div>
-            </div>
-            {/* <img
-              src={images[1].src}
-              className="block border border-white"
-              alt=""
-            /> */}
-          </div>
-        </div>
-        <div className="w-full h-[900px]"></div>
-        <div className={st.divisor}>
-          <SVGWhiteDots />
-        </div>
-      </section>
+      <GamesAvailableSection />
     </>
   )
 }
-
-// {images.map(i => (
-//   <div
-//     key={i.src}
-//     className="relative overflow-hidden basis-[34%] grow w-[34%]"
-//   >
-//     <div className="relative overflow-hidden">
-//       <img
-//         src={i.src}
-//         alt=""
-//         // className="invisible"
-//         // className="fillimg"
-//       />
-//     </div>
-//   </div>
-// ))}
