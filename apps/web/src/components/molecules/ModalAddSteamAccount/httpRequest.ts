655604acafaebf36b4966d3bdffd6ae68090056f
Vitor Markis <vitormarkis2369@gmail.com>
2024-01-03 23:37:40 -0300

    fix: handle if http request throws

--- apps/web/src/components/molecules/ModalAddSteamAccount/httpRequest.ts
+++ apps/web/src/components/molecules/ModalAddSteamAccount/httpRequest.ts
@@ -10,12 +10,18 @@ export async function httpCreateSteamAccount(
   // if (payload.authCode) return [null, "892j38429-93=459"]
   // return [new ApplicationError("Steam Guard needed.", 202), null]
 
-  const api = await getAPI()
-  const response = await api.post<any, AxiosResponse<AddSteamAccountOutput>, CreateSteamAccountPayload>(
-    "/steam-accounts",
-    payload
-  )
-  if (response.status === 202) return [new ApplicationError("Steam Guard needed.", 202), null]
-  if (response.status === 201) return [null, response.data.steamAccountID]
-  return [new ApplicationError("Erro desconhecido.", 500), null]
+  try {
+    const api = await getAPI()
+    const response = await api.post<any, AxiosResponse<AddSteamAccountOutput>, CreateSteamAccountPayload>(
+      "/steam-accounts",
+      payload
+    )
+    if (response.status === 202) return [new ApplicationError("Steam Guard needed.", 202), null]
+    if (response.status === 201) return [null, response.data.steamAccountID]
+    console.log({ response })
+    return [new ApplicationError("Erro desconhecido.", 500), null]
+  } catch (error) {
+    console.log({ error })
+    return [new ApplicationError(error.message, 500), null]
+  }
 }
